64fb804d3dc260a95ae803d2ddad085f1b038ea8
robsv <svirskasr@janelia.hhmi.org>
2020-07-31 10:30:53 -0400

    Added profile support and S3FileSystem error checking

--- bin/s3cp.py
+++ bin/s3cp.py
@@ -21,16 +21,19 @@ def fwalk(source: str, endswith='') -> Tuple[List[str], int, int]:
     """
     results = []
     considered = total_size = 0
-    for p, d, f in os.walk(source):
-        for file in f:
+    for wpath, wdir, files in os.walk(source):
+        for file in files:
             considered += 1
             if file.endswith(endswith):
-                fpath = os.path.join(p, file)
+                fpath = os.path.join(wpath, file)
                 total_size += os.stat(fpath).st_size
                 results.append(fpath)
     return results, considered, total_size
 
+
 def humansize(num: int, suffix='B') -> str:
+    ''' Convert a storage number to a human-readable string
+    '''
     for unit in ['', 'K', 'M', 'G', 'T']:
         if abs(num) < 1024.0:
             return "%3.1f%s%s" % (num, unit, suffix)
@@ -38,11 +41,18 @@ def humansize(num: int, suffix='B') -> str:
     return "%.1f%s%s" % (num, 'P', suffix)
 
 
-def iterput(sources: Sequence[str], dests: Sequence[str], tags: Sequence[Optional[dict]]):
+def iterput(sources: Sequence[str], dests: Sequence[str], tags: Sequence[Optional[dict]], profile):
     """
     Given a sequence of sources, dests, and tags, save each source to dest with a tag.
     """
-    fs = s3fs.S3FileSystem(anon=False)
+    try:
+        if profile:
+            fs = s3fs.S3FileSystem(anon=False, profile=profile)
+        else:
+            fs = s3fs.S3FileSystem(anon=False)
+    except Exception as err:
+        print(TEMPLATE.format(type(err).__name__, err.args))
+        sys.exit(-1)
     if tags:
         for source, dest, tag in zip(sources, dests, tags):
             try:
@@ -63,7 +73,8 @@ def iterput(sources: Sequence[str], dests: Sequence[str], tags: Sequence[Optiona
     return True
 
 
-def s3put(dest_root: str, source_path: str, dryrun: bool, endswith: Optional[str] = '', tags: Optional[dict] = None, **kwargs):
+def s3put(dest_root: str, source_path: str, dryrun: bool, profile: Optional[str] = None,
+          endswith: Optional[str] = '', tags: Optional[dict] = None, **kwargs):
     sources, considered, total_size = fwalk(source_path, endswith)
     if dryrun:
         for fpath in sources:
@@ -76,9 +87,10 @@ def s3put(dest_root: str, source_path: str, dryrun: bool, endswith: Optional[str
 
     source_bag = bag.from_sequence(sources)
     dest_bag = bag.from_sequence(dests)
-    tag_bag = bag.from_sequence((tags,) * len(sources)) if len(tags) else None
+    tag_bag = bag.from_sequence((tags,) * len(sources)) if tags else None
 
-    return bag.map_partitions(iterput, source_bag, dest_bag, tag_bag), len(sources), total_size
+    return bag.map_partitions(iterput, source_bag, dest_bag, tag_bag, profile), \
+                              len(sources), total_size
 
 
 @click.command()
@@ -91,12 +103,14 @@ def s3put(dest_root: str, source_path: str, dryrun: bool, endswith: Optional[str
 @click.option('-dvt', '--developer-tag', default=None, type=str)
 @click.option('-pt', '--project-tag', default=None, type=str)
 @click.option('-dt', '--description-tag', default=None, type=str)
+@click.option('-pr', '--profile', default=None, type=str)
 @click.option('-dr', '--dryrun', default=False, is_flag=True)
 @click.option('-vb', '--verbose', default=False, is_flag=True)
 @click.option('-db', '--debug', default=False, is_flag=True)
 
 
-def s3put_cli(source_paths, bucket, workers, endswith, version_tag, stage_tag, developer_tag, project_tag, description_tag, dryrun, verbose, debug):
+def s3put_cli(source_paths, bucket, workers, endswith, version_tag, stage_tag,
+              developer_tag, project_tag, description_tag, profile, dryrun, verbose, debug):
     LOGGER = colorlog.getLogger()
     if debug:
         LOGGER.setLevel(colorlog.colorlog.logging.DEBUG)
@@ -121,7 +135,8 @@ def s3put_cli(source_paths, bucket, workers, endswith, version_tag, stage_tag, d
         if len(source_paths) > 1:
             print("Source " + source_path)
         dest_root = Path(bucket) / Path(source_path).stem
-        result, source_count, source_size = s3put(dest_root, source_path, endswith=endswith, tags=tags, dryrun=dryrun)
+        result, source_count, source_size = s3put(dest_root, source_path, endswith=endswith,
+                                                  tags=tags, profile=profile, dryrun=dryrun)
         total['count'] += source_count
         total['size'] += source_size
         if result:
@@ -133,12 +148,13 @@ def s3put_cli(source_paths, bucket, workers, endswith, version_tag, stage_tag, d
         print("Total files: %d" % total['count'])
         print("Total size: %s" % humansize(total['size']))
         if not dryrun:
-            print("Data transfer rate: %.2f MB/sec" % (total['size'] / total['time'] / (1024 * 1024)))
+            print("Data transfer rate: %.2f MB/sec"
+                  % (total['size'] / total['time'] / (1024 * 1024)))
 
 if __name__ == '__main__':
     if not (sys.version_info[0] == 3 and sys.version_info[1] >= 6):
         print("This program requires at least Python 3.6")
         sys.exit(-1)
-    pbar = ProgressBar()
-    pbar.register()
+    PBAR = ProgressBar()
+    PBAR.register()
     s3put_cli()
